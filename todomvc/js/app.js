9dedba51ce60d9d90abb7e8960f98e0cfc065f3a
Stefan Haustein <stefan.haustein@gmail.com>
2019-12-07 23:18:04 +0100

    Cleanup

--- todomvc/js/app.js
+++ todomvc/js/app.js
@@ -1,220 +1,140 @@
 (function (window) {
-	'use strict';
+  'use strict';
 
-	// Model
+  // Model
 
-	let currentFilter = "";  // Not part of the persistent state.
-	let state = JSON.parse(localStorage.getItem("state")) || {
-		text: "",
-		items: [],
-	};
+  let currentFilter = "";  // Not part of the persistent state.
+  let state = JSON.parse(localStorage.getItem("state")) || {
+    text: "",
+    items: [],
+  };
 
-	// Controller
+  // Controller
 
-	window.onhashchange = updateFilter;
+  window.onhashchange = updateFilter;
 
-	function countOpenItems() {
-		let result = 0;
-		state.items.forEach(item => result += item.completed ? 0 : 1);
-		return result;
-	}
+  function countOpenItems() {
+    let result = 0;
+    state.items.forEach(item => result += item.completed ? 0 : 1);
+    return result;
+  }
 
-	function updateFilter() {
-		currentFilter = window.location.hash.substr(2);
-		render();
-	}
+  function updateFilter() {
+    currentFilter = window.location.hash.substr(2);
+    render();
+  }
 
-	function addItem(event) {
-		event.preventDefault();
-		state.items.push({text: state.text});
-		update();
-		document.querySelector(".new-todo").focus();
-	}
+  function addItem(event) {
+    event.preventDefault();
+    state.items.push({text: state.text});
+    update();
+    document.querySelector(".new-todo").focus();
+  }
 
-	function toggleItem(index) {
-		state.items[index].completed = !state.items[index].completed;
-		update();
-	}
+  function toggleItem(index) {
+    state.items[index].completed = !state.items[index].completed;
+    update();
+  }
 
-	function destroyItem(index) {
-		state.items.splice(index, 1);
-		update();
-	}
+  function destroyItem(index) {
+    state.items.splice(index, 1);
+    update();
+  }
 
-	function saveState() {
-		localStorage.setItem("state", JSON.stringify(state));
-	}
+  function saveState() {
+    localStorage.setItem("state", JSON.stringify(state));
+  }
 
-	function update() {
-		saveState();
-		render();
-	}
+  function update() {
+    saveState();
+    render();
+  }
 
-	function setAll(completed) {
-		state.items.forEach(item => item.completed = completed);
-		update();
-	}
+  function setAll(completed) {
+    state.items.forEach(item => item.completed = completed);
+    update();
+  }
 
-	// View
+  // View
 
-	function renderTodoList() {
-		return element("ul", {class: "todo-list"}, state.items.map((item, index) => renderItem(item, index)));
-	}
+  function renderTodoList() {
+    return element("ul", {class: "todo-list"}, state.items.map((item, index) => renderItem(item, index)));
+  }
 
-	function renderItem(item, index) {
-		if (currentFilter == (item.completed ? "active" : "completed")) {
-			return null;
-		}
-		return element("li",
-			(item.completed ? {"class": "completed"} : null),
-			{dblclick: event => {
-					let li = event.target.closest("li");
-					li.classList.add("editing");
-					li.querySelector(".edit").focus();
-			}},
-		 	element("div", {"class": "view"},
-				element("input", {
-					class: "toggle",
-					type: "checkbox",
-					checked: item.completed ? "checked" : null,
-					change: () => toggleItem(index)}),
-				element("label", item.text),
-				element("button", {"class": "destroy"}, () => destroyItem(index))),
-			element("input", {
-				class: "edit",
-				value: item.text,
-				change: e => {
-					item.text = e.target.value;
-					saveState()
-				},
-				blur: render}));
-	}
+  function renderItem(item, index) {
+    if (currentFilter == (item.completed ? "active" : "completed")) {
+      return null;
+    }
+    return element("li",
+      (item.completed ? {"class": "completed"} : null),
+      {dblclick: event => {
+          let li = event.target.closest("li");
+          li.classList.add("editing");
+          li.querySelector(".edit").focus();
+      }},
+       element("div", {"class": "view"},
+        element("input", {
+          class: "toggle",
+          type: "checkbox",
+          checked: item.completed ? "checked" : null,
+          change: () => toggleItem(index)}),
+        element("label", item.text),
+        element("button", {"class": "destroy"}, () => destroyItem(index))),
+      element("input", {
+        class: "edit",
+        value: item.text,
+        change: e => {
+          item.text = e.target.value;
+          saveState()
+        },
+        blur: render}));
+  }
 
-	/*
-	<ul class="todo-list">
-		<!-- These are here just to show the structure of the list items -->
-		<!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-		<li class="completed">
-			<div class="view">
-				<input class="toggle" type="checkbox" checked>
-				<label>Taste JavaScript</label>
-				<button class="destroy"></button>
-			</div>
-			<input class="edit" value="Create a TodoMVC template">
-		</li>
-		<li>
-			<div class="view">
-				<input class="toggle" type="checkbox">
-				<label>Buy a unicorn</label>
-				<button class="destroy"></button>
-			</div>
-			<input class="edit" value="Rule the web">
-		</li>
-	</ul>
+  function renderMain() {
+    return state.items.length == 0 ? null :
+      element("section", {class: "main"},
+        element("input", {
+            id: "toggle-all",
+            class: "toggle-all",
+            checked: countOpenItems() == 0 ? "checked" : null,
+            type: "checkbox",
+            click: event => setAll(event.target.checked)}),
+        element("label", {for: "toggle-all"}, "Mark all as complete"),
+        renderTodoList(state.items));
+  }
 
-	*/
-
-	function renderMain() {
-		return state.items.length == 0 ? null :
-			element("section", {class: "main"},
-				element("input", {
-						id: "toggle-all",
-						class: "toggle-all",
-						checked: countOpenItems() == 0 ? "checked" : null,
-						type: "checkbox",
-						click: event => setAll(event.target.checked)}),
-				element("label", {for: "toggle-all"}, "Mark all as complete"),
-				renderTodoList(state.items));
-	}
-
-	function renderFooter() {
-		return state.items.length == 0 ? null :
-			element("footer", {class: "footer"},
-				element("span", {class: "todo-count"},
-					element("strong", countOpenItems()),
-					" items left"),
-				element("ul",
-					{class: "filters"},
-					 [["All", ""], ["Active", "active"], ["Completed", "completed"]].map(([label, key]) =>
-						element("li",
-							element("a", {
-								href: "#/" + key,
-								class: currentFilter == key ? "selected" : ""},
-							label)))));
-	}
-
-/*
-<!-- This should be `0 items left` by default -->
-<span class="todo-count"><strong>0</strong> item left</span>
-<!-- Remove this if you don't implement routing -->
-<ul class="filters">
-	<li>
-		<a class="selected" href="#/">All</a>
-	</li>
-	<li>
-		<a href="#/active">Active</a>
-	</li>
-	<li>
-		<a href="#/completed">Completed</a>
-	</li>
-</ul>
-<!-- Hidden if no completed items are left ↓ -->
-<button class="clear-completed">Clear completed</button>
-*/
+  function renderFooter() {
+    return state.items.length == 0 ? null :
+      element("footer", {class: "footer"},
+        element("span", {class: "todo-count"},
+          element("strong", countOpenItems()),
+          " items left"),
+        element("ul",
+          {class: "filters"},
+           [["All", ""], ["Active", "active"], ["Completed", "completed"]].map(([label, key]) =>
+            element("li",
+              element("a", {
+                href: "#/" + key,
+                class: currentFilter == key ? "selected" : ""},
+              label)))));
+  }
 
 
-	function render() {
+  function render() {
+    document.getElementsByClassName("todoapp")[0].replaceWith(
+      element("section", {"class": "todoapp"},
+        element("header", {"class": "header"},
+          element("h1", "todos"),
+          element("form", {submit: event => addItem(event)},
+            element("input", {
+              class: "new-todo",
+              autofocus: "autofocus",
+              placeholder: "What needs to be done?",
+              change: event => state.text = event.target.value}))),
+        renderMain(),
+        renderFooter()));
+  }
 
-		console.log("currentFitler: ", currentFilter);
-		document.getElementsByClassName("todoapp")[0].replaceWith(
-			element("section", {"class": "todoapp"},
-				element("header", {"class": "header"},
-					element("h1", "todos"),
-					element("form", {submit: event => addItem(event)},
-						element("input", {
-							class: "new-todo",
-							autofocus: "autofocus",
-							placeholder: "What needs to be done?",
-							change: event => state.text = event.target.value}))),
-				renderMain(),
-				renderFooter()));
-	}
-
-	updateFilter();
-
-/*
-
-
-	<section class="todoapp">
-		<header class="header">
-			<h1>todos</h1>
-			<input class="new-todo" placeholder="What needs to be done?" autofocus>
-		</header>
-		<!-- This section should be hidden by default and shown when there are todos -->
-		<section class="main">
-			<input id="toggle-all" class="toggle-all" type="checkbox">
-			<label for="toggle-all">Mark all as complete</label>
-		</section>
-		<!-- This footer should hidden by default and shown when there are todos -->
-		<footer class="footer">
-			<!-- This should be `0 items left` by default -->
-			<span class="todo-count"><strong>0</strong> item left</span>
-			<!-- Remove this if you don't implement routing -->
-			<ul class="filters">
-				<li>
-					<a id="filter-all" class="selected" href="#/">All</a>
-				</li>
-				<li>
-					<a href="#/active">Active</a>
-				</li>
-				<li>
-					<a href="#/completed">Completed</a>
-				</li>
-			</ul>
-			<!-- Hidden if no completed items are left ↓ -->
-			<button class="clear-completed">Clear completed</button>
-		</footer>
-
-*/
+  updateFilter();
 
 })(window);
--- todomvc/readme.md
+++ todomvc/readme.md
@@ -1,19 +1,13 @@
-# TodoMVC App Template
+# NoTemplate • [TodoMVC](http://todomvc.com)
 
-> Template used for creating [TodoMVC](http://todomvc.com) apps
+The best templating system is no templating system! :)
 
-![](https://github.com/tastejs/todomvc-app-css/raw/master/screenshot.png)
+## Resources
 
+- [Github](https://github.com/stefanhaustein/notemplate)
 
-## Getting started
+### Support
 
-- Read the [Application Specification](https://github.com/tastejs/todomvc/blob/master/app-spec.md) before touching the template.
+- [Twitter](http://twitter.com/dukoid)
 
-- Delete this file and rename `app-readme.md` to `readme.md` and fill it out.
-
-- Clone this repo and install the dependencies with [npm](https://npmjs.com) by running: `npm install`.
-
-
-## License
-
-<a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />This <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" rel="dct:type">work</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://sindresorhus.com" property="cc:attributionName" rel="cc:attributionURL">TasteJS</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US">Creative Commons Attribution 4.0 International License</a>.
+*Let us [know](https://github.com/stefanhaustein/notemplate/issues) if you discover anything worth sharing.*
